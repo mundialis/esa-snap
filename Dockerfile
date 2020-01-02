dcb399f032c7a9bbdbbd01fdfbc3e27fa2ca54fd
Carmen Tawalika <tawalika@mundialis.de>
2020-01-02 16:14:51 +0100

    make snappy run for alpine

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,7 @@ LABEL maintainer="tawalika@mundialis.de,neteler@mundialis.de"
 USER root
 
 ENV BUILD_PACKAGES="\
+      gawk \
       gcc \
       git \
       maven \
@@ -15,9 +16,11 @@ ENV BUILD_PACKAGES="\
       "
 
 ENV PACKAGES="\
-      openjdk11 \
+      fontconfig \
+      openjdk8 \
       python3 \
       vim \
+      ttf-dejavu \
       zip \
       "
 
@@ -27,11 +30,12 @@ RUN echo "Install dependencies and tools";\
     apk add --no-cache $PACKAGES; \
     echo "Install step done"
 
-
 ENV LC_ALL "en_US.UTF-8"
 
 # install SNAPPY
-ENV JAVA_HOME "/usr/lib/jvm/java-11-openjdk"
-ENV PATH $PATH:/usr/lib/jvm/java-11-openjdk/bin
+ENV JAVA_HOME "/usr/lib/jvm/java-1.8-openjdk"
+# SNAP wants the current folder '.' included in LD_LIBRARY_PATH
+ENV LD_LIBRARY_PATH ".:$LD_LIBRARY_PATH"
+
 COPY snap /src/snap
 RUN sh /src/snap/install.sh
--- snap/install.sh
+++ snap/install.sh
@@ -2,16 +2,13 @@
 
 # https://senbox.atlassian.net/wiki/spaces/SNAP/pages/30539778/Install+SNAP+on+the+command+line
 # https://senbox.atlassian.net/wiki/spaces/SNAP/pages/30539785/Update+SNAP+from+the+command+line
-
 # http://step.esa.int/main/download/snap-download/
-SNAPVER=7
 
-# set JAVA_HOME (done in Docker as well)
-export JAVA_HOME=/usr/lib/jvm/java-11-openjdk
+SNAPVER=7
 
 # install module 'jpy' (A bi-directional Python-Java bridge)
 git clone https://github.com/bcdev/jpy.git /src/snap/jpy
-pip3 install wheel
+pip3 install --upgrade pip wheel
 (cd /src/snap/jpy && python3 setup.py bdist_wheel)
 # hack because ./snappy-conf will create this dir but also needs *.whl files...
 mkdir -p /root/.snap/snap-python/snappy
@@ -20,7 +17,19 @@ cp /src/snap/jpy/dist/*.whl "/root/.snap/snap-python/snappy"
 # install and update snap
 wget -q -O /src/snap/esa-snap_all_unix_${SNAPVER}_0.sh \
   "http://step.esa.int/downloads/${SNAPVER}.0/installers/esa-snap_all_unix_${SNAPVER}_0.sh"
+
+# hacks to make it run on alpine
+sed -i 's+ bin/unpack200+ $JAVA_HOME/bin/unpack200+g' /src/snap/esa-snap_all_unix_${SNAPVER}_0.sh
+# more hacks to use system java and not oracle (included in installer).
+# Oracle JAVA is not supported with alpine due to missing glibc.
+sed -i 's+$INSTALL4J_JAVA_PREFIX "$app_java_home/bin/java"+$INSTALL4J_JAVA_PREFIX java+g' /src/snap/esa-snap_all_unix_${SNAPVER}_0.sh
+sed -i 's+-Dinstall4j.jvmDir="$app_java_home"+-Dinstall4j.jvmDir=$JAVA_HOME/jre+g' /src/snap/esa-snap_all_unix_${SNAPVER}_0.sh
+sed -i 's+-Djava.ext.dirs="$app_java_home/lib/ext:$app_java_home/jre/lib/ext"++g' /src/snap/esa-snap_all_unix_${SNAPVER}_0.sh
+sed -i 's+-classpath "$local_classpath"+-classpath "$local_classpath:$app_java_home/lib/ext:$app_java_home/jre/lib/ext"+g' /src/snap/esa-snap_all_unix_${SNAPVER}_0.sh
 sh /src/snap/esa-snap_all_unix_${SNAPVER}_0.sh -q -varfile /src/snap/response.varfile
+
+# one more hack to keep using system java
+sed -i 's+jdkhome="./jre"+jdkhome="$JAVA_HOME"+g' /usr/local/snap/etc/snap.conf
 /usr/local/snap/bin/snap --nosplash --nogui --modules --update-all
 
 # create snappy and python binding with snappy
@@ -32,3 +41,7 @@ sh /src/snap/esa-snap_all_unix_${SNAPVER}_0.sh -q -varfile /src/snap/response.va
 
 # cleanup installer
 rm -f /src/snap/esa-snap_all_unix_${SNAPVER}_0.sh
+
+################################################################################
+# keep for debugging
+# export INSTALL4J_KEEP_TEMP=yes
