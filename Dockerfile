0c2770d9e2b157dbe258dcd18eaf368411f660dd
Carmen Tawalika <tawalika@mundialis.de>
2020-01-03 16:02:36 +0100

    clean after merge + make image smaller

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM alpine:edge
+FROM alpine:edge as build
 
 LABEL authors="Carmen Tawalika,Markus Neteler"
 LABEL maintainer="tawalika@mundialis.de,neteler@mundialis.de"
@@ -33,11 +33,18 @@ RUN echo "Install dependencies and tools";\
 ENV LC_ALL "en_US.UTF-8"
 # SNAP wants the current folder '.' included in LD_LIBRARY_PATH
 ENV LD_LIBRARY_PATH ".:$LD_LIBRARY_PATH"
-
 # install SNAPPY
 ENV JAVA_HOME "/usr/lib/jvm/java-1.8-openjdk"
-# SNAP wants the current folder '.' included in LD_LIBRARY_PATH
-ENV LD_LIBRARY_PATH ".:$LD_LIBRARY_PATH"
 
 COPY snap /src/snap
 RUN sh /src/snap/install.sh
+
+
+FROM alpine:edge
+
+RUN apk add openjdk8 python3
+ENV LD_LIBRARY_PATH ".:$LD_LIBRARY_PATH"
+COPY --from=build /root/.snap /root/.snap
+COPY --from=build /usr/local/snap /usr/local/snap
+RUN (cd /root/.snap/snap-python/snappy && python3 setup.py install)
+RUN /usr/bin/python3 -c 'from snappy import ProductIO'
--- snap/install.sh
+++ snap/install.sh
@@ -5,6 +5,8 @@
 # http://step.esa.int/main/download/snap-download/
 
 SNAPVER=7
+# avoid NullPointer crash during S-1 processing
+java_max_mem=10G
 
 # install module 'jpy' (A bi-directional Python-Java bridge)
 git clone https://github.com/bcdev/jpy.git /src/snap/jpy
@@ -31,6 +33,7 @@ sh /src/snap/esa-snap_all_unix_${SNAPVER}_0.sh -q -varfile /src/snap/response.va
 # one more hack to keep using system java
 sed -i 's+jdkhome="./jre"+jdkhome="$JAVA_HOME"+g' /usr/local/snap/etc/snap.conf
 /usr/local/snap/bin/snap --nosplash --nogui --modules --update-all
+rm -rf /usr/local/snap/jre
 
 # create snappy and python binding with snappy
 /usr/local/snap/bin/snappy-conf /usr/bin/python3
@@ -38,10 +41,6 @@ sed -i 's+jdkhome="./jre"+jdkhome="$JAVA_HOME"+g' /usr/local/snap/etc/snap.conf
 
 # increase the JAVA VM size to avoid NullPointer exception in Snappy during S-1 processing
 (cd /root/.snap/snap-python/snappy && sed -i "s/^java_max_mem:.*/java_max_mem: $java_max_mem/" snappy.ini)
-
-# get minor python version
-PYMINOR=$(python3 -c 'import platform; major, minor, patch = platform.python_version_tuple(); print(minor)')
-(cd /usr/local/lib/python3.$PYMINOR/dist-packages/snappy/ && sed -i "s/^java_max_mem:.*/java_max_mem: $java_max_mem/" snappy.ini)
 
 # test
 /usr/bin/python3 -c 'from snappy import ProductIO'
